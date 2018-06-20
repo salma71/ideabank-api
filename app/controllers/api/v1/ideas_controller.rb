3a6b8f52a2ae60d4182c2a013110872941e81167
Salma Elshaelshahawy <salma71@gmail.com>
2018-06-20 12:37:09 -0400

    add CRUD actions to the ideas controller

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -2,7 +2,36 @@ module Api::V1
   class IdeasController < ApplicationController
     def index
       @ideas = Idea.all
-      render json: @ideas
+
+      render json: @ideas, status: 200
+    end
+
+    def show
+        @idea = Idea.find(params[:id])
+
+        render json: @idea, status: 200
+    end
+    def create
+        @idea = Idea.create(idea_params)
+
+        render json: @idea, status: 200
+    end
+    def update
+        @idea = Idea.find(params[:id])
+        @idea.update(idea_params)
+
+        render json: @idea, status: 200
+    end
+    def destory
+        @idea = Idea.find(params[:id])
+        @idea.delete
+
+        render json: {ideaId: @idea.id}
+    end
+
+    private 
+    def idea_params
+        params.require(:idea).permit(:title, :body)
     end
   end
 end
